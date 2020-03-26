2b858f9e8a7ae369ce80c7d5c2d7b3ee8b40217b
Lasse Blaauwbroek <lasse@blaauwbroek.eu>
2020-03-26 13:49:36 +0100

    work around Coq bug

--- theories/Ltac1Dummy.v
+++ theories/Ltac1Dummy.v
@@ -1,4 +1,4 @@
-From Ltac2 Require Import Control Notations.
+From Ltac2 Require Import Control Notations Ltac1.
 
 Ltac2 rec prove_with' tacs :=
 match tacs with
@@ -6,24 +6,25 @@ match tacs with
 | t::tacs' => focus 1 1 t; prove_with' tacs'
 end.
 
-Ltac2 prove_with tacs reprover :=
+Ltac searcher := fail
+  "A previously synthesized proof script by Tactician has failed."
+  "In order to re-synthesize, the Tactician plugin must be installed."
+  "Check https://coq-tactician.github.io for instructions".
+
+Ltac2 prove_with tacs s :=
 plus 
   (fun () => complete (fun () => prove_with' tacs))
-  (fun e => reprover).
+  (fun e => run s).
 
-Ltac2 dummy_reprover tacs := prove_with tacs ltac1:(fail 
-    "A previously synthesized proof script by Tactician has failed."
-    "In order to re-synthesize, the Tactician plugin must be installed."
-    "Check https://coq-tactician.github.io for instructions").
+(* A hack to work around https://github.com/coq/coq/issues/11866 *)
+Ltac2 Notation "-" tacs(list0(thunk(self), "-")) "--|" := prove_with tacs.
 
-Ltac2 Notation "[#" tacs(list0(thunk(self), "#")) "#]" : 1 := dummy_reprover tacs.
+Tactic Notation (at level 4) "search" "failing" tactic(t) := t searcher.
 
-Tactic Notation (at level 4) "search" "failing" tactic(t) := t.
+Tactic Notation (at level 4) "search" := fail 
+  "Tactic 'search' requires the Tactician plugin to be installed."
+  "Check https://coq-tactician.github.io for instructions".
 
-Tactic Notation (at level 4) "search" :=
-fail "Tactic 'search' requires the Tactician plugin to be installed."
-     "Check https://coq-tactician.github.io for instructions".
-
-Tactic Notation (at level 4) "suggest" :=
-idtac "Tactic 'suggest' requires the Tactician plugin to be installed."
-      "Check https://coq-tactician.github.io for instructions.".
+Tactic Notation (at level 4) "suggest" := idtac
+  "Tactic 'suggest' requires the Tactician plugin to be installed."
+  "Check https://coq-tactician.github.io for instructions.".
