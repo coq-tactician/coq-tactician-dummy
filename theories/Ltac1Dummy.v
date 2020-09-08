d46f4042d07363b3eabc140eb9862387e0cc394a
Lasse Blaauwbroek <lasse@blaauwbroek.eu>
2020-09-08 21:09:57 +0200

    Lower tactic precedences (works better with try).

--- theories/Ltac1Dummy.v
+++ theories/Ltac1Dummy.v
@@ -5,12 +5,12 @@ Ltac searcher := fail
   "In order to re-synthesize, the Tactician plugin must be installed."
   "Check https://coq-tactician.github.io for instructions".
 
-Tactic Notation (at level 4) "search" "failing" tactic(t) := solve [t] || searcher.
+Tactic Notation (at level 1) "search" "failing" tactic(t) := solve [t] || searcher.
 
-Tactic Notation (at level 4) "search" := fail 
+Tactic Notation (at level 1) "search" := fail 
   "Tactic 'search' requires the Tactician plugin to be installed."
   "Check https://coq-tactician.github.io for instructions".
 
-Tactic Notation (at level 4) "suggest" := idtac
+Tactic Notation (at level 1) "suggest" := idtac
   "Tactic 'suggest' requires the Tactician plugin to be installed."
   "Check https://coq-tactician.github.io for instructions.".
