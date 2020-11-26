91bd1caf392702865a947415d422f27eac87d4a1
Lasse Blaauwbroek <lasse@blaauwbroek.eu>
2020-11-26 07:07:25 +0100

    Another attempt at making sure the real tactician takes precedence

--- theories/Ltac1Dummy.v
+++ theories/Ltac1Dummy.v
@@ -1,12 +1,15 @@
 Declare ML Module "ltac_plugin".
 
-Ltac searcher := fail
+(* These notations are put on a lower level so that the real Tactician can override it.
+   Note that tactic redefinition does not work, because this module might be loaded _later_
+   than the real Tactician. *)
+Tactic Notation (at level 4) "search" "with" "cache" tactic3(t) := solve [t] || (fail
   "A previously synthesized proof script by Tactician has failed."
   "In order to re-synthesize, the Tactician plugin must be installed."
-  "Check https://coq-tactician.github.io for instructions".
+  "Check https://coq-tactician.github.io for instructions").
 
-Tactic Notation "search" "with" "cache" tactic3(t) := solve [t] || searcher.
-
-Ltac search := fail
+Tactic Notation (at level 4) "search" := fail
   "Tactic 'search' requires the Tactician plugin to be installed."
   "Check https://coq-tactician.github.io for instructions".
+
+Tactic Notation (at level 4) "tactician" "ignore" tactic3(t) := t.
